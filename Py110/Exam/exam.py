5bc54c687f2edc64775bad87c8ddaecb3cf8dadc
ShishkinAndrey <shishkin1994@yandex.ru>
2020-12-15 13:32:52 +0300

    First commit

---
+++ Py110/Exam/exam.py
@@ -0,0 +1,139 @@
+import random
+import re
+import json
+import csv
+
+import argparse
+from faker import Faker
+
+import conf
+
+PK_FILE = 'books.txt'
+AUTHOR_FILE = 'authors.txt'
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--count', type=int, required=True, help='Кол-во случайных элементов, которое необходимо сгенерировать')
+    parser.add_argument('--authors', type=int, required=False, help='Кол-во авторов для каждого элемента')
+    parser.add_argument('--sale', type=int, required=False, help='Скидка')
+    parser.add_argument('--output', required=False, help='Формат вывода(.json / .csv')
+    subparsers = parser.add_subparsers(dest='json_csv')
+    save_json = subparsers.add_parser('json', help='Введите json для записи в .json файл')
+    save_json.add_argument('-jsfn', required=False, type=argparse.FileType('w'), help='Имя .json файла')
+    save_json.add_argument('--indent', required=False, type=int, default=0, help='Размер отступов')
+    save_csv = subparsers.add_parser('csv', help='Введите csv для записи в .json файл')
+    save_csv.add_argument('--csvname', required=False, type=argparse.FileType('w'), help='Имя .csv файла')
+    return parser
+#
+#
+# def subparsers_json(subparsers):
+#     save_json = subparsers.add_parser('json', help='Введите json для записи в .json файл')
+#     save_json.add_argument('-jsfn', required=False, type=argparse.FileType('w'), help='Имя .json файла')
+#     save_json.add_argument('--indent', required=False, type=int, default=0, help='Размер отступов')
+#     return save_json
+#
+#
+# def subparsers_csv(subparsers):
+#     save_csv = subparsers.add_parser('csv', help='Введите csv для записи в .json файл')
+#     save_csv.add_argument('--csvname', required=False, type=argparse.FileType('w'), help='Имя .csv файла')
+#
+#     return save_csv
+
+def get_authors():
+    '''
+    Функция, выполняющая проверку файла authors.txt на соответствие регулярному выражению
+    :return: список, содержащий корректные строки
+    '''
+    reg_author = re.compile(r'\b[A-Z][a-z]*\b\s[A-Z][a-z]*\b')
+    with open(AUTHOR_FILE, 'rt') as f:
+        author_list = []
+        i = 0
+        for line in f:
+            i += 1
+            if re.match(reg_author, line):
+                author_list.append(line[:line.index('\n')])
+            else:
+                print('Строка', line[:line.index("\n")], 'номер', i, 'не прошла проверку', )
+    return author_list
+
+
+def gen_book():
+    dict_book = {}
+    pk_value = 1
+    title_number = 0
+    fake = Faker()
+    author_list = get_authors()
+    while True:
+        dict_book['model'] = conf.model
+        dict_book['pk'] = pk_value
+        dict_book['fields'] = {}
+        with open(PK_FILE, 'rt') as f:
+            title_list = f.read().splitlines()
+        dict_book['fields']['title'] = title_list[title_number]
+        dict_book['fields']['year'] = random.randint(1900, 2020)
+        dict_book['fields']['pages'] = random.randint(1, 1000)
+        dict_book['fields']['isbn13'] = fake.isbn13()
+        dict_book['fields']['rating'] = round(random.uniform(0, 5), 1)
+        dict_book['fields']['price'] = round(random.uniform(10, 100), 2)
+        if args.sale:
+            dict_book['fields']['discount'] = args.sale
+        else:
+            dict_book['fields']['discount'] = random.randint(1, 100)
+        if args.authors:
+            dict_book['fields']['author'] = [random.choice(author_list) for _ in range(args.authors)]
+        else:
+            dict_book['fields']['author'] = [random.choice(author_list) for _ in range(random.randint(1, 3))]
+        yield dict_book
+        pk_value += 1
+        title_number += 1
+
+def csv_writer(path, fieldnames, data):
+    """
+    Функция для записи в файл csv
+    path - путь до файла
+    fieldnames - название столбцов
+    data - список из списков
+    """
+    with open(path, "a", newline='') as out_file:
+        '''
+        out_file - выходные данные в виде объекта
+        delimiter - разделитель :|;
+        fieldnames - название полей (столбцов)
+        '''
+        writer = csv.DictWriter(out_file, delimiter=';', fieldnames=fieldnames)
+        # writer = csv.writer(out_file, delimiter=';', fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+
+if __name__ == '__main__':
+    result = gen_book()
+    parser = create_parser()
+    args = parser.parse_args()
+    if args.json_csv == 'json':
+        for _ in range(args.count):
+            json.dump(next(result), args.jsfn, indent=args.indent)
+    if args.json_csv == 'csv':
+        my_list = []
+        list_fieldnames = []
+        i = 0
+        updated_dict = next(result)
+        for fieldnames in updated_dict.keys():
+            list_fieldnames.append(fieldnames)
+        for fields_keys in updated_dict.get('fields').keys():
+            list_fieldnames.append(fields_keys)
+        # работающий вариант!!!
+        while i != args.count:
+            # updated_dict = next(result)
+            updated_dict.update(updated_dict.pop('fields'))
+            print(updated_dict)
+            my_list.append(updated_dict)
+            csv_writer(args.csvname.name, list_fieldnames, my_list)
+            my_list.clear()
+            i += 1
+            updated_dict = next(result)
+    elif not args.json_csv:
+        for _ in range(args.count):
+            print(next(result))
